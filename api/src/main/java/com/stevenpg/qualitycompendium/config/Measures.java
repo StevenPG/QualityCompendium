753297d21300c8284b7c2963f09779dde82a7d94
StevenPG <steven.p.gantz@gmail.com>
2020-02-08 10:37:35 -0500

    Removal some deprecated classes/methods, converted to single-yaml for testing

--- api/src/main/java/com/stevenpg/qualitycompendium/config/Measures.java
+++ api/src/main/java/com/stevenpg/qualitycompendium/config/Measures.java
@@ -24,38 +24,4 @@ public class Measures {
                     "sqale_rating",
                     "alert_status"
             );
-
-    @Deprecated(since = "2.0", forRemoval = true)
-    public static final List<String> configuredMeasuresGroupingOne =
-            List.of(
-                    "complexity",
-                    "duplicated_blocks",
-                    "violations",
-                    "code_smells",
-                    "sqale_rating",
-                    "sqale_index",
-                    "alert_status",
-                    "bugs",
-                    "reliability_rating",
-                    "reliability_remediation_effort",
-                    "vulnerabilities",
-                    "security_remediation_effort",
-                    "classes"
-                    );
-
-    @Deprecated(since = "2.0", forRemoval = true)
-    public static final List<String> configuredMeasuresGroupingTwo =
-            List.of(
-                    "comment_lines",
-                    "security_rating",
-                    "lines",
-                    "ncloc",
-                    "coverage",
-                    "new_coverage",
-                    "uncovered_lines",
-                    "tests",
-                    "test_errors",
-                    "test_failures",
-                    "test_success_density"
-            );
 }
